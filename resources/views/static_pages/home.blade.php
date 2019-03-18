ceb41c6f9874654d5653d14d51a348ede0a0bf10
vagrant <vagrant@homestead>
2019-03-18 02:13:12 +0000

    命名路由

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -10,12 +10,8 @@
     <p>
       一切，将从这里开始。
     </p>
-    <p>
-      一切，将从这里开始。
-    </p>
-    <p>
-      一切，将从这里开始。
-    </p>
+    
+   
     <p>
       <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
     </p>
