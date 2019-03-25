e5e308265b2e4b1122432a6090170cd00a383a35
vagrant <vagrant@homestead>
2019-03-25 06:37:21 +0000

    关注按钮

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -35,9 +37,12 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::resource('statuses',  'StatusesController', ['only' => ['store', 'destroy']]);
 
 /**
- * 用户关注列表和粉丝列表
- * 
+ * 用户关注列表和粉丝列表,
+ * 关注用户和取消关注
  */
 Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
 Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
 
+
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
