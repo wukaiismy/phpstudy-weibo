c103ea2e6598ba6b5de4aa9245c99fa8b0fb2e25
vagrant <vagrant@homestead>
2019-03-25 03:08:11 +0000

    社交统计信息

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,18 +1,21 @@
-@extends('layouts.default') 
-@section('title','主页') 
+@extends('layouts.default')
+@section('title','主页')
 @section('content') @if (Auth::check())
 <div class="row">
   <div class="col-md-8">
     <section class="status_form">
-  @include('shared._status_form')
+      @include('shared._status_form')
     </section>
     <h4>微博列表</h4>
     <hr>
-  @include('shared._feed')
+    @include('shared._feed')
   </div>
   <aside class="col-md-4">
     <section class="user_info">
-  @include('shared._user_info', ['user' => Auth::user()])
+      @include('shared._user_info', ['user' => Auth::user()])
+    </section>
+    <section class="stats mt-2">
+      @include('shared._stats', ['user' => Auth::user()])
     </section>
   </aside>
 </div>
@@ -29,5 +32,5 @@
     <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
   </p>
 </div>
-@endif 
+@endif
 @stop
