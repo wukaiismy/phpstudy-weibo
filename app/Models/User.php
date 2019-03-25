929cf505aedb3ed9e47dcb756ea1d95b263cdb8a
vagrant <vagrant@homestead>
2019-03-25 06:50:13 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Notifications\ResetPassword;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -78,8 +79,9 @@ class User extends Authenticatable
      */
     public function feed()
     {
-        return $this->statuses()
-            ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
     }
     /**
      * 粉丝数据关联表
